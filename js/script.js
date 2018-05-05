a416c01431ac37adae51c611926aa92c1f9d4e51
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2018-05-05 18:17:26 -0500

    v3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,48 @@
+/**
+* Aqui comienza la parte mi script
+* Dueño: raul peinado
+*/
 function mostrarBuffet1(){
-	alert("hola");
+	var x = document.getElementById("pant");
+	x.setAttribute('src', 'buffet1.html')
 }
 
+//creacion de productos por buffet
+var listaBuffet1=[];
+
+var objcomida1={
+	"nombre":"Pollo a la Brasa",
+	"cantidad": 12,
+	"cocinero":"Marco Buleggi"
+	"costo": 18.5
+	"Calidad": "premium"
+}
+var objcomida1={
+	"nombre":"Arroz con pollo",
+	"cantidad": 12,
+	"cocinero":"Marco Buleggi"
+	"costo": 18.5
+	"Calidad": "premium"
+}
+
+var objcomida1={
+	"nombre":"Pollo a la Brasa",
+	"cantidad": 12,
+	"cocinero":"Marco Buleggi"
+	"costo": 18.5
+	"Calidad": "premium"
+}
+
+var objcomida1={
+	"nombre":"Pollo a la Brasa",
+	"cantidad": 12,
+	"cocinero":"Marco Buleggi"
+	"costo": 18.5
+	"Calidad": "premium"
+}
+
+
+/**
+* Termina la parte mi script
+* Dueño: raul peinado
+*/
